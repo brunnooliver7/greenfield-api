df9951c69bb2501715c41bf194fb09b48a707492
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-08-23 18:08:45 -0300

    spring initializer

---
+++ src/main/java/com/greenfieldapi/GreenfieldApiApplication.java
@@ -0,0 +1,13 @@
+package com.greenfieldapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenfieldApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenfieldApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenfieldapi/GreenfieldApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfieldapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenfieldApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
