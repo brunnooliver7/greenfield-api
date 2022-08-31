9513d1f7afc8aff5b4f78b3b81f1117f580f5c8f
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-08-31 13:19:05 -0300

    mapeamentos JPA - Medicamento

--- src/main/java/com/greenfieldapi/Model/Medicamento.java
+++ src/main/java/com/greenfieldapi/Model/Medicamento.java
@@ -1,13 +1,15 @@
 package com.greenfieldapi.Model;
 
-import java.math.BigDecimal;
-
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,13 +17,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "medicamento")
+@Table(name = "tb_medicamento")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Medicamento {
-  
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
@@ -29,11 +31,16 @@ public class Medicamento {
   private String descricao;
 
   private Integer quantidade;
-  
-  private BigDecimal dosagem;
-  
+
+  private String dosagem;
+
   private String frequencia;
 
   @ManyToOne
+  @JsonIgnore
+  @JoinColumn(name = "prescricao_id", nullable = false)
   private Prescricao prescricao;
+
+  @Column(name = "prescricao_id", insertable = false, updatable = false)
+  private Long prescricaoId;
 }
