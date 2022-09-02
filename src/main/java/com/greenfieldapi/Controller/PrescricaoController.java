bd87a19eed52fe65e9c721b21d842e58c7073960
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-09-02 20:19:14 -0300

    finalizando CRUD incial de Prescricao

--- src/main/java/com/greenfieldapi/Controller/PrescricaoController.java
+++ src/main/java/com/greenfieldapi/Controller/PrescricaoController.java
@@ -2,13 +2,17 @@ package com.greenfieldapi.Controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.greenfieldapi.DTO.PrescricaoDTO;
+import com.greenfieldapi.Mapper.PrescricaoMapper;
 import com.greenfieldapi.Model.Prescricao;
 import com.greenfieldapi.Service.PrescricaoService;
 
@@ -18,26 +22,43 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/prescricao")
 @RequiredArgsConstructor
 public class PrescricaoController {
-  
+
   private final PrescricaoService prescricaoService;
 
   @GetMapping("/medico/{medicoId}")
-  public List<Prescricao> findByMedicoId(@PathVariable Long medicoId) {
-    return prescricaoService.findByMedicoId(medicoId);
+  public List<PrescricaoDTO> findByMedicoId(@PathVariable Long medicoId) {
+    List<Prescricao> prescricoes = prescricaoService.findByMedicoId(medicoId);
+    return PrescricaoMapper.INSTANCE.toDTOs(prescricoes);
   }
 
   @GetMapping("/paciente/{pacienteId}")
-  public List<Prescricao> findByPacienteId(@PathVariable Long pacienteId) {
-    return prescricaoService.findByPacienteId(pacienteId);
+  public List<PrescricaoDTO> findByPacienteId(@PathVariable Long pacienteId) {
+    List<Prescricao> prescricoes = prescricaoService.findByPacienteId(pacienteId);
+    return PrescricaoMapper.INSTANCE.toDTOs(prescricoes);
   }
 
   @GetMapping("/{id}")
-  public Prescricao findById(@PathVariable Long id) {
-    return prescricaoService.findById(id);
+  public PrescricaoDTO findById(@PathVariable Long id) {
+    Prescricao prescricao = prescricaoService.findById(id);
+    return PrescricaoMapper.INSTANCE.toDTO(prescricao);
   }
 
   @PostMapping
-  public Prescricao save(@RequestBody Prescricao prescricao) {
-   return prescricaoService.save(prescricao);
-  }  
+  public PrescricaoDTO save(@RequestBody PrescricaoDTO prescricaoDTO) {
+    Prescricao prescricao = PrescricaoMapper.INSTANCE.toEntity(prescricaoDTO);
+    prescricao = prescricaoService.save(prescricao);
+    return PrescricaoMapper.INSTANCE.toDTO(prescricao);
+  }
+
+  @PutMapping
+  public PrescricaoDTO update(@RequestBody PrescricaoDTO prescricaoDTO) {
+    Prescricao prescricao = PrescricaoMapper.INSTANCE.toEntity(prescricaoDTO);
+    prescricao = prescricaoService.update(prescricao);
+    return PrescricaoMapper.INSTANCE.toDTO(prescricao);
+  }
+
+  @DeleteMapping("/{id}")
+  public void delete(@PathVariable Long id) {
+    prescricaoService.delete(id);
+  }
 }
--- src/main/java/com/greenfieldapi/Service/PrescricaoService.java
+++ src/main/java/com/greenfieldapi/Service/PrescricaoService.java
@@ -4,9 +4,6 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import com.greenfieldapi.Model.Medicamento;
-import com.greenfieldapi.Model.Medico;
-import com.greenfieldapi.Model.Paciente;
 import com.greenfieldapi.Model.Prescricao;
 import com.greenfieldapi.Repository.PrescricaoRepository;
 
@@ -17,9 +14,6 @@ import lombok.RequiredArgsConstructor;
 public class PrescricaoService {
 
   private final PrescricaoRepository prescricaoRepository;
-  private final MedicoService medicoService;
-  private final PacienteService pacienteService;
-  private final MedicamentoService medicamentoService;
 
   public List<Prescricao> findByMedicoId(Long medicoId) {
     return prescricaoRepository.findByMedicoId(medicoId);
@@ -34,24 +28,14 @@ public class PrescricaoService {
   }
 
   public Prescricao save(Prescricao prescricao) {
-    Medico medico = medicoService.findById(prescricao.getMedicoId());
-    prescricao.setMedico(medico);
+    return prescricaoRepository.save(prescricao);
+  }
 
-    Paciente paciente = pacienteService.findById(prescricao.getPacienteId());
-    prescricao.setPaciente(paciente);
+  public Prescricao update(Prescricao prescricao) {
+    return prescricaoRepository.save(prescricao);
+  }
 
-    Prescricao prescricaoSalva = prescricaoRepository.save(prescricao);
-
-    List<Medicamento> medicamentosSalvos = prescricao.getMedicamentos().stream()
-        .map(medicamento -> {
-          medicamento.setPrescricao(prescricaoSalva);
-          medicamento.setPrescricaoId(prescricaoSalva.getId());
-          return medicamentoService.save(medicamento);
-        })
-        .toList();
-
-    prescricaoSalva.setMedicamentos(medicamentosSalvos);
-
-    return prescricaoSalva;
+  public void delete(Long id) {
+    prescricaoRepository.deleteById(id);
   }
 }
