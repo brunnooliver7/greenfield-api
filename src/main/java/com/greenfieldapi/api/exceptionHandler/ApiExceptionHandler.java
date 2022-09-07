3d49d37b357f893e2430a2295b7affe3e48576cf
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-09-07 18:29:09 -0300

    handleMethodArgumentTypeMismatch

--- src/main/java/com/greenfieldapi/api/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/greenfieldapi/api/exceptionHandler/ApiExceptionHandler.java
@@ -3,6 +3,7 @@ package com.greenfieldapi.api.exceptionHandler;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
@@ -90,6 +92,33 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     return handleExceptionInternal(ex, exceptionBody, headers, status, request);
   }
+
+  @Override
+	protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+		
+		if (ex instanceof MethodArgumentTypeMismatchException) {
+			return handleMethodArgumentTypeMismatch(
+					(MethodArgumentTypeMismatchException) ex, headers, status, request);
+		}
+	
+		return super.handleTypeMismatch(ex, headers, status, request);
+	}
+	
+	private ResponseEntity<Object> handleMethodArgumentTypeMismatch(
+			MethodArgumentTypeMismatchException ex, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+
+		ExceptionType exceptionType = ExceptionType.PARAMETRO_INVALIDO;
+
+		String detail = String.format("O parâmetro de URL '%s' recebeu o valor '%s', "
+				+ "que é de um tipo inválido. Corrija e informe um valor compatível com o tipo %s.",
+				ex.getName(), ex.getValue(), ex.getRequiredType().getSimpleName());
+
+		ExceptionBody exceptionBody = createExceptionBuilder(status, exceptionType, detail).build();
+
+		return handleExceptionInternal(ex, exceptionBody, headers, status, request);
+	}
 
   @Override
   protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
--- src/main/java/com/greenfieldapi/api/exceptionHandler/ExceptionType.java
+++ src/main/java/com/greenfieldapi/api/exceptionHandler/ExceptionType.java
@@ -8,7 +8,8 @@ public enum ExceptionType {
 	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
   ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
 	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
-  MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível");
+  MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível"),
+	PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido");
 
 	private String title;
 	private String uri;
