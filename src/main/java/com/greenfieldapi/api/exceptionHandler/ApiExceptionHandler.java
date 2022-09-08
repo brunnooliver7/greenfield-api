a73ef319645493bed2e5aa4f61a83b19259fbd32
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-09-08 18:50:38 -0300

    Erro de sistema

--- src/main/java/com/greenfieldapi/api/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/greenfieldapi/api/exceptionHandler/ApiExceptionHandler.java
@@ -22,6 +22,20 @@ import com.greenfieldapi.domain.exception.EntidadeNaoEncontrada.EntidadeNaoEncon
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<Object> handleUncaught(Exception ex, WebRequest request) {
+    HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+    ExceptionType exceptionType = ExceptionType.ERRO_DE_SISTEMA;
+    String detail = "Ocorreu um erro interno inesperado no sistema. "
+        + "Tente novamente e se o problema persistir, entre em contato "
+        + "com o administrador do sistema.";
+    ex.printStackTrace();
+
+    ExceptionBody exceptionBody = createExceptionBuilder(status, exceptionType, detail).build();
+
+    return handleExceptionInternal(ex, exceptionBody, new HttpHeaders(), status, request);
+  }
+
   @ExceptionHandler(NegocioException.class)
   public ResponseEntity<?> handleNegocioException(NegocioException ex, WebRequest request) {
 
@@ -94,31 +108,31 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
   }
 
   @Override
-	protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers,
-			HttpStatus status, WebRequest request) {
-		
-		if (ex instanceof MethodArgumentTypeMismatchException) {
-			return handleMethodArgumentTypeMismatch(
-					(MethodArgumentTypeMismatchException) ex, headers, status, request);
-		}
-	
-		return super.handleTypeMismatch(ex, headers, status, request);
-	}
-	
-	private ResponseEntity<Object> handleMethodArgumentTypeMismatch(
-			MethodArgumentTypeMismatchException ex, HttpHeaders headers,
-			HttpStatus status, WebRequest request) {
+  protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers,
+      HttpStatus status, WebRequest request) {
 
-		ExceptionType exceptionType = ExceptionType.PARAMETRO_INVALIDO;
+    if (ex instanceof MethodArgumentTypeMismatchException) {
+      return handleMethodArgumentTypeMismatch(
+          (MethodArgumentTypeMismatchException) ex, headers, status, request);
+    }
 
-		String detail = String.format("O parâmetro de URL '%s' recebeu o valor '%s', "
-				+ "que é de um tipo inválido. Corrija e informe um valor compatível com o tipo %s.",
-				ex.getName(), ex.getValue(), ex.getRequiredType().getSimpleName());
+    return super.handleTypeMismatch(ex, headers, status, request);
+  }
 
-		ExceptionBody exceptionBody = createExceptionBuilder(status, exceptionType, detail).build();
+  private ResponseEntity<Object> handleMethodArgumentTypeMismatch(
+      MethodArgumentTypeMismatchException ex, HttpHeaders headers,
+      HttpStatus status, WebRequest request) {
 
-		return handleExceptionInternal(ex, exceptionBody, headers, status, request);
-	}
+    ExceptionType exceptionType = ExceptionType.PARAMETRO_INVALIDO;
+
+    String detail = String.format("O parâmetro de URL '%s' recebeu o valor '%s', "
+        + "que é de um tipo inválido. Corrija e informe um valor compatível com o tipo %s.",
+        ex.getName(), ex.getValue(), ex.getRequiredType().getSimpleName());
+
+    ExceptionBody exceptionBody = createExceptionBuilder(status, exceptionType, detail).build();
+
+    return handleExceptionInternal(ex, exceptionBody, headers, status, request);
+  }
 
   @Override
   protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
