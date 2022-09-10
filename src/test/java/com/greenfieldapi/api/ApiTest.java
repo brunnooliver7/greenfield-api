8aefc0492b2c7613cceba1cd5b7f3a559de3cb1c
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-09-10 20:35:22 -0300

    removendo dependencias, classes e metodos desnecessarios para maniulacao de JSON

--- src/test/java/com/greenfieldapi/api/ApiTest.java
+++ src/test/java/com/greenfieldapi/api/ApiTest.java
@@ -1,25 +1,11 @@
 package com.greenfieldapi.api;
 
-import java.lang.reflect.Type;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParseException;
-import com.google.gson.JsonPrimitive;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
 
 import io.restassured.RestAssured;
 import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
@@ -40,31 +26,4 @@ public class ApiTest {
     RestAssured.port = port;
   }
 
-  public String criarJson(Object obj) {
-    GsonBuilder gsonBuilder = new GsonBuilder();
-    gsonBuilder.registerTypeAdapter(LocalDate.class, new LocalDateSerializer());
-    gsonBuilder.registerTypeAdapter(LocalDate.class, new LocalDateDeserializer());
-    
-    Gson gson = gsonBuilder.create();
-    return gson.toJson(obj);
-  }
-
-}
-
-class LocalDateSerializer implements JsonSerializer<LocalDate> {
-  private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
-  @Override
-  public JsonElement serialize(LocalDate localDate, Type srcType, JsonSerializationContext context) {
-      return new JsonPrimitive(formatter.format(localDate));
-  }
-}
-
-class LocalDateDeserializer implements JsonDeserializer<LocalDate> {
-  @Override
-  public LocalDate deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
-  throws JsonParseException {
-      return LocalDate.parse(json.getAsString(),
-          DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-  }
 }
