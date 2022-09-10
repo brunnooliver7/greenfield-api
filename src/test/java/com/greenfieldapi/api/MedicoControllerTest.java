6b094915b010764597c1176c7f2d73c11a609842
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-09-10 12:57:59 -0300

    api: deve_obter_todos_os_medicos

--- src/test/java/com/greenfieldapi/api/MedicoControllerTest.java
+++ src/test/java/com/greenfieldapi/api/MedicoControllerTest.java
@@ -1,22 +1,29 @@
 package com.greenfieldapi.api;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
 import com.greenfieldapi.domain.model.Medico;
+import com.greenfieldapi.domain.repository.MedicoRepository;
 
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 
 public class MedicoControllerTest extends ApiTest {
 
+  @Autowired
+  MedicoRepository medicoRepository;
+  
   @Test
-  void deveCriarMedico() throws Exception {
+  void deve_criar_medico() throws Exception {
     given()
-      .body(criarJson(criarMedico()))
+      .body(criarJson(criarMedico("91354036085", "a@email.com", "001")))
       .contentType(ContentType.JSON)
       .accept(ContentType.JSON)
     .when()
@@ -25,17 +32,40 @@ public class MedicoControllerTest extends ApiTest {
       .statusCode(HttpStatus.CREATED.value());
   }
 
-  private Medico criarMedico() {
+  @Test
+  public void deve_obter_todos_os_medicos() {
+
+    medicoRepository.save(criarMedico("91354036085", "a@email", "001"));
+    medicoRepository.save(criarMedico("58896718040", "b@email", "002"));
+    medicoRepository.save(criarMedico("44981367058", "c@email", "003"));
+
+    Response response = 
+      given()
+        .accept(ContentType.JSON)
+      .when()
+        .get("/medico")
+      .then()
+        .extract()
+        .response();
+
+    assertEquals(HttpStatus.OK.value(), response.statusCode());
+    
+    int size = response.jsonPath().getList("id").size();
+    assertEquals(3, size);
+  }
+
+  private Medico criarMedico(String cpf, String email, String crm) {
     return Medico.builder()
-      .cpf("07978344002")
-      .email("email@example.com")
+      .cpf(cpf)
+      .email(email)
       .nome("nome")
       .dtNascimento(LocalDate.of(2000, 1, 1))
-      .crm("crm")
+      .crm(crm)
       .estadoRegistroCrm("estadoRegistroCrm")
       .estado("ES")
       .sexo("F")
       .senha("senha")
       .build();
   }
+
 }
