7da83cfd65214700d8ab8a9878172c840f26031e
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-09-10 20:34:43 -0300

    api: deve_alterar_um_paciente

--- src/test/java/com/greenfieldapi/api/PacienteApiTest.java
+++ src/test/java/com/greenfieldapi/api/PacienteApiTest.java
@@ -1,20 +1,26 @@
 package com.greenfieldapi.api;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 import java.time.LocalDate;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-
-import static io.restassured.RestAssured.given;
 
 import com.greenfieldapi.api.dto.PacienteDTO;
 import com.greenfieldapi.api.mapper.PacienteMapper;
 import com.greenfieldapi.domain.model.Paciente;
+import com.greenfieldapi.domain.repository.PacienteRepository;
 
 import io.restassured.http.ContentType;
 
 public class PacienteApiTest extends ApiTest {
   
+  @Autowired
+  private PacienteRepository pacienteRepository;
+
   @Test
   public void deve_criar_um_paciente() {
     
@@ -29,6 +35,27 @@ public class PacienteApiTest extends ApiTest {
       .post("/paciente")
     .then()
       .statusCode(HttpStatus.CREATED.value());
+  }
+
+  @Test
+  public void deve_alterar_um_paciente() {
+
+    Paciente paciente = pacienteRepository.save(
+      criarPaciente("91354036085")
+    );
+
+    PacienteDTO dto = PacienteMapper.INSTANCE.toDTO(paciente);
+    dto.setNome("nome edit");
+
+    given()
+      .body(dto)
+      .contentType(ContentType.JSON)
+      .accept(ContentType.JSON)
+    .when()
+      .put("/paciente")
+    .then()
+      .statusCode(HttpStatus.OK.value())
+      .body("nome", equalTo(dto.getNome()));
   }
 
   private Paciente criarPaciente(String cpf) {
