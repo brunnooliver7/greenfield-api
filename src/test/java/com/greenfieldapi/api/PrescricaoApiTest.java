1414b4ed0b0bb83f94c87283e9b813b6bd7dad72
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-09-13 22:24:30 -0300

    api: deve_obter_todas_as_prescricoes_de_um_medico

--- src/test/java/com/greenfieldapi/api/PrescricaoApiTest.java
+++ src/test/java/com/greenfieldapi/api/PrescricaoApiTest.java
@@ -3,15 +3,21 @@ package com.greenfieldapi.api;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
+import java.util.Arrays;
+
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
 import com.greenfieldapi.api.dto.PrescricaoDTO;
 import com.greenfieldapi.api.mapper.PrescricaoMapper;
+import com.greenfieldapi.domain.model.Medicamento;
 import com.greenfieldapi.domain.model.Medico;
+import com.greenfieldapi.domain.model.Paciente;
 import com.greenfieldapi.domain.model.Prescricao;
 import com.greenfieldapi.domain.repository.MedicoRepository;
+import com.greenfieldapi.domain.repository.PacienteRepository;
 import com.greenfieldapi.domain.repository.PrescricaoRepository;
 
 import io.restassured.http.ContentType;
@@ -23,6 +29,9 @@ public class PrescricaoApiTest extends ApiTest {
 
   @Autowired
   private MedicoRepository medicoRepository;
+
+  @Autowired
+  private PacienteRepository pacienteRepository;
 
   @Test
   public void deve_criar_uma_prescricao() {
@@ -63,4 +72,39 @@ public class PrescricaoApiTest extends ApiTest {
       .body("medicoId", equalTo(dto.getMedicoId().intValue()));
   }
 
+  @Test
+  public void deve_obter_todas_as_prescricoes_de_um_medico() {
+    Medico medico = criarMedico("58896718040", "a@email", "001");
+    medico = medicoRepository.save(medico);
+
+    Paciente paciente = criarPaciente("44981367058");
+    paciente = pacienteRepository.save(paciente);
+
+    Medicamento medicamento1 = criarMedicamento();
+
+    Medicamento medicamento2 = criarMedicamento();
+
+    Prescricao prescricao1 = Prescricao.builder()
+      .medicoId(medico.getId())
+      .pacienteId(paciente.getId())
+      .medicamentos(Arrays.asList(medicamento1))
+      .build();
+
+    Prescricao prescricao2 = Prescricao.builder()
+      .medicoId(medico.getId())
+      .pacienteId(paciente.getId())
+      .medicamentos(Arrays.asList(medicamento2))
+      .build();
+
+    prescricaoRepository.save(prescricao1);
+    prescricaoRepository.save(prescricao2);
+
+      given()
+        .accept(ContentType.JSON)
+      .when()
+        .get("prescricao/medico/" + medico.getId())
+      .then()
+        .body("", Matchers.hasSize(2))
+        .statusCode(HttpStatus.OK.value());
+  }
 }
