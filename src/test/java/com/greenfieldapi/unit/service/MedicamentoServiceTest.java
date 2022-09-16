8387420e855efa9ca17f06cb4c0d8d226f3b04b2
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-09-16 19:42:35 -0300

    unit: deve_criar_um_medicamento (service)

---
+++ src/test/java/com/greenfieldapi/unit/service/MedicamentoServiceTest.java
@@ -0,0 +1,42 @@
+package com.greenfieldapi.unit.service;
+
+import static com.greenfieldapi.TestUtils.criarMedicamento;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.greenfieldapi.domain.model.Medicamento;
+import com.greenfieldapi.domain.repository.MedicamentoRepository;
+import com.greenfieldapi.domain.service.MedicamentoService;
+
+@ExtendWith(MockitoExtension.class)
+public class MedicamentoServiceTest {
+  
+  @InjectMocks
+  private MedicamentoService medicamentoService;
+
+  @Mock
+  private MedicamentoRepository medicamentoRepository;
+
+  @Test
+  public void deve_criar_um_medicamento() {
+    Medicamento medicamento = criarMedicamento();
+
+    when(medicamentoRepository.save(medicamento)).thenReturn(medicamento);
+
+    medicamento = medicamentoService.save(medicamento);
+
+    assertNotNull(medicamento);
+  }
+
+}
