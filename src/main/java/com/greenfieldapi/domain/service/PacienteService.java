138a1f0e5ff4dab5c77225195ff623e0a60aa1e1
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-09-20 19:39:44 -0300

    log PacienteService

--- src/main/java/com/greenfieldapi/domain/service/PacienteService.java
+++ src/main/java/com/greenfieldapi/domain/service/PacienteService.java
@@ -11,29 +11,35 @@ import com.greenfieldapi.domain.model.Paciente;
 import com.greenfieldapi.domain.repository.PacienteRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PacienteService {
   
   private final PacienteRepository pacienteRepository;
 
   public List<Paciente> findAll() {
+    log.info("Buscando todos os pacientes");
     return pacienteRepository.findAll();
   }
 
   public Paciente findById(Long id) {
+    log.info(String.format("Buscando paciente (id = %d)", id));
     return pacienteRepository.findById(id).orElseThrow(() -> new PacienteNaoEncontradoException(id));
   }
 
   @Transactional
   public Paciente save(Paciente paciente) {
+    log.info("Salvando paciente");
     return pacienteRepository.save(paciente);
   }
 
   @Transactional
   public void delete(Long id) {
     try {
+      log.info(String.format("Deletando paciente (id = %d)", id));
       pacienteRepository.deleteById(id);
       pacienteRepository.flush();
     } catch (EmptyResultDataAccessException e) {
