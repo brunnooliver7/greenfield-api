a3aedae0c94d7489c3893cde1630146596fdae2e
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-09-20 19:38:22 -0300

    log MedicoService

--- src/main/java/com/greenfieldapi/domain/service/MedicoService.java
+++ src/main/java/com/greenfieldapi/domain/service/MedicoService.java
@@ -11,29 +11,35 @@ import com.greenfieldapi.domain.model.Medico;
 import com.greenfieldapi.domain.repository.MedicoRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MedicoService {
 
   private final MedicoRepository medicoRepository;
 
   public List<Medico> findAll() {
+    log.info("Buscando todos os medicos");
     return medicoRepository.findAll();
   }
 
   public Medico findById(Long id) {
+    log.info(String.format("Buscando medico (id = %d)", id));
     return medicoRepository.findById(id).orElseThrow(() -> new MedicoNaoEncontradoException(id));
   }
 
   @Transactional
   public Medico save(Medico medico) {
+    log.info("Salvando medico");
     return medicoRepository.save(medico);
   }
-
+  
   @Transactional
   public void delete(Long id) {
     try {
+      log.info(String.format("Deletando medico (id = %d)", id));
       medicoRepository.deleteById(id);
       medicoRepository.flush();
     } catch (EmptyResultDataAccessException e) {
